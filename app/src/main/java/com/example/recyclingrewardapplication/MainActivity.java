15f71330fdab05acdd721e2e594e90f1d82dfa94
StigkasPantelis <stigkaspantelis@gmail.com>
2024-05-17 15:28:06 +0300

    SignIn page

--- app/src/main/java/com/example/recyclingrewardapplication/MainActivity.java
+++ app/src/main/java/com/example/recyclingrewardapplication/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_in_fragment);
+        setContentView(R.layout.activity_main);
     }
 
 
