36f5cb2ed51d3be04324361014d03c7f1a355cc4
LAPTOP-O54P5DFT\koulis_antonis <sousloglou1914@gmail.com>
2024-05-24 17:00:42 +0300

    activityform and activityprofile

---
+++ app/src/main/java/com/example/recyclingrewardapplication/ProfileFragment.java
@@ -0,0 +1,16 @@
+package com.example.recyclingrewardapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class ProfileFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_fragment);
+
+    }
+}
