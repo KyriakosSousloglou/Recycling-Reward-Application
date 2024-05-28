18f699f2c6e6d943544c77fe796c1f8c9fdeb737
LAPTOP-O54P5DFT\koulis_antonis <sousloglou1914@gmail.com>
2024-05-28 14:23:35 +0300

    profile activity

--- app/src/main/java/com/example/recyclingrewardapplication/LoginFragment.java
+++ app/src/main/java/com/example/recyclingrewardapplication/LoginFragment.java
@@ -5,16 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class LoginFragment extends AppCompatActivity {
+
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_fragment);
+        txt = findViewById(R.id.notRegister);
+        txt.setClickable(true);
     }
     public void onButtonLoginFragmentLoginClick(View v){
-        Intent intent = new Intent(LoginFragment.this, FormFragment.class);
+        Intent intent = new Intent(LoginFragment.this, ProfileFragment.class);
+        startActivity(intent);
+    }
+
+    public void onClickNotRegister(View v){
+        Intent intent = new Intent(LoginFragment.this, SignInFragment.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/recyclingrewardapplication/ProfileFragment.java
+++ app/src/main/java/com/example/recyclingrewardapplication/ProfileFragment.java
@@ -6,11 +6,12 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class ProfileFragment extends AppCompatActivity {
-
+    private ImageView avatar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_fragment);
-
+        avatar = findViewById(R.id.avatar);
+        avatar.setImageResource(R.drawable.avatar_image);
     }
 }
