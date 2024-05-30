bc9f0a8e59eeafb91201ae98c262dc03b94c8e91
StigkasPantelis <stigkaspantelis@gmail.com>
2024-05-30 12:43:08 +0300

    kalispera

--- app/src/main/java/com/example/recyclingrewardapplication/LoginFragment.java
+++ app/src/main/java/com/example/recyclingrewardapplication/LoginFragment.java
@@ -6,27 +6,137 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
 
 public class LoginFragment extends AppCompatActivity {
 
     TextView txt;
 
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_fragment);
-        txt = findViewById(R.id.notRegister);
-        txt.setClickable(true);
-    }
-    public void onClickLogin(View v){
-        Intent intent = new Intent(LoginFragment.this, ProfileFragment.class);
-        startActivity(intent);
+
+        usernameEditText = findViewById(R.id.username_text);
+        passwordEditText = findViewById(R.id.editTextTextPassword2);
+        loginButton = findViewById(R.id.button_login_fragment_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser();
+            }
+        });
     }
 
-    public void onClickNotRegister(View v){
-        Intent intent = new Intent(LoginFragment.this, SignInFragment.class);
-        startActivity(intent);
+    private void loginUser() {
+        final String username = usernameEditText.getText().toString().trim();
+        final String password = passwordEditText.getText().toString().trim();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(LoginFragment.this, "Please enter username and password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://192.168.2.7/recycling/login.php");
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("POST");
+                    httpURLConnection.setDoOutput(true);
+
+                    OutputStreamWriter writer = new OutputStreamWriter(httpURLConnection.getOutputStream());
+                    writer.write("username=" + username + "&password=" + password);
+                    writer.flush();
+                    writer.close();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                    final StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    reader.close();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                JSONObject jsonResponse = new JSONObject(stringBuilder.toString());
+                                String status = jsonResponse.getString("status");
+                                String message = jsonResponse.getString("message");
+
+                                Toast.makeText(LoginFragment.this, message, Toast.LENGTH_SHORT).show();
+
+                                if (status.equals("success")) {
+                                    Intent intent = new Intent(LoginFragment.this, ProfileFragment.class);
+                                    startActivity(intent);
+                                    // Handle successful login (e.g., open a new activity)
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                Toast.makeText(LoginFragment.this, "Error parsing response", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(LoginFragment.this, "Error connecting to server", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        }).start();
     }
+
+
+
+
+
+
+
+
+
+
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login_fragment);
+//        txt = findViewById(R.id.notRegister);
+//        txt.setClickable(true);
+//    }
+//    public void onClickLogin(View v){
+//        Intent intent = new Intent(LoginFragment.this, ProfileFragment.class);
+//        startActivity(intent);
+//    }
+//
+//    public void onClickNotRegister(View v){
+//        Intent intent = new Intent(LoginFragment.this, SignInFragment.class);
+//        startActivity(intent);
+//    }
 
     public void onBackButtonClick(View view) {
         onBackPressed();
--- app/src/main/java/com/example/recyclingrewardapplication/SignInFragment.java
+++ app/src/main/java/com/example/recyclingrewardapplication/SignInFragment.java
@@ -3,20 +3,114 @@ package com.example.recyclingrewardapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class SignInFragment extends AppCompatActivity {
+
+    private EditText username_txt, password_txt, email_txt, phone_txt;
+    private Button button_signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_fragment);
+
+        username_txt = findViewById(R.id.username_signIn);
+        password_txt = findViewById(R.id.password_signIn);
+        email_txt = findViewById(R.id.email_signIn);
+        phone_txt = findViewById(R.id.phone_signIn);
+        button_signIn = findViewById(R.id.button_signIn);
+
+        button_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = username_txt.getText().toString().trim();
+                String password = password_txt.getText().toString().trim();
+                String phone = phone_txt.getText().toString().trim();
+                String email = email_txt.getText().toString().trim();
+                new RegisterTask().execute(username,password, email, phone);
+            }
+        });
     }
-    public void onClickSignIn(View v){
-        Toast.makeText(this, "Your account has been created successfully", Toast.LENGTH_LONG).show();
+
+    private class RegisterTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String username = params[0];
+            String password = params[1];
+            String email = params[2];
+            String phone = params[3];
+            String url = "http://192.168.2.7/recycling/register.php"; // Αντικαταστήστε με την πραγματική διεύθυνση του PHP script
+
+            try {
+                // Δημιουργία αιτήματος HTTP POST
+                URL registerUrl = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection) registerUrl.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+
+                // Αποστολή δεδομένων στον server
+                String postData = "&username=" + URLEncoder.encode(username, "UTF-8") +
+                        "&password=" + URLEncoder.encode(password, "UTF-8") +
+                        "&email=" + URLEncoder.encode(email, "UTF-8")+
+                        "&phone=" + URLEncoder.encode(phone, "UTF-8");
+
+                OutputStream outputStream = connection.getOutputStream();
+                outputStream.write(postData.getBytes());
+                outputStream.flush();
+                outputStream.close();
+
+                // Ανάγνωση της απάντησης από τον server
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                // Επιστροφή της απάντησης ως συμβολοσειρά
+                return response.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                Toast.makeText(SignInFragment.this, result, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(SignInFragment.this, "Failed to connect to server", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
+
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_sign_in_fragment);
+//    }
+//    public void onClickSignIn(View v){
+//        Toast.makeText(this, "Your account has been created successfully", Toast.LENGTH_LONG).show();
+//    }
     public void onBackButtonClick(View view) {
         onBackPressed();
     }
